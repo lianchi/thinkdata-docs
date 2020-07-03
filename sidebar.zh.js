bd40e697b48265e03aadd19c7b2ef314bba12165
zhoulianchi <zhoulianchi@706.com>
2020-07-03 15:17:01 +0800

    init

---
+++ sidebar.zh.js
@@ -0,0 +1,35 @@
+var I18N_ZH = {
+    language: "简体中文",
+    editLinkText: "在 GitHub 上编辑此页",
+    nav: [{
+            title: "首页",
+            link: "/zh/README.md"
+        },
+        {
+            title: "更新日志",
+            link: "/CHANGELOG"
+        },
+        {
+            title: "GitHub",
+            link: "https://github.com/saekiraku/vscode-rainbow-fart"
+        }
+    ],
+    sidebar: [
+        {
+            title: "概述",
+            link: "/zh/README.md"
+        },
+        {
+            title: "更多炫酷彩虹屁",
+            link: "/zh/awesome-rainbow-fart.md"
+        },
+        {
+            title: "常见问题",
+            link: "/zh/qa.md"
+        },
+        {
+            title: "定制语音包",
+            link: "/zh/voice-packages.md"
+        }
+    ]
+}
